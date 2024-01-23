bd08717b5bedeb31f0e0534ce86fc202d0816499
Xbox Series S <xboxseriess@Olivias-MBP.attlocal.net>
2024-01-23 00:55:30 -0500

    Header component styled

--- src/components/Header/headerOptions.tsx
+++ src/components/Header/headerOptions.tsx
@@ -1,16 +1,57 @@
-import { Facebook, Instagram } from '@mui/icons-material';
+// import { useTransition } from 'react';
+import { Facebook, Instagram } from '@mui/icons-material'
+import styled from 'styled-components'
+
+import { headerOptionsMenu } from '../../../en.json'
+
+const StyledHeaderOptions = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    padding: 0px 6%;
+    background-color: #353B21;
+    padding: 10px;
+`;
+
+const StyledNavBar = styled.div`
+    margin-left: 5%;
+`;
+const StyledHeaderItem = styled.a`
+    text-decoration: none;
+    margin-right: 30px;
+    color: #FFFF;
+    font-family: sans-serif;
+    font-size: 12px;
+`;
+
+const StyledSocialIcons = styled.div`
+    margin-right: 10%;
+`;
+
+const StyledFacebook = styled(Facebook)`
+    margin-right: 30px;
+    color: #FFFF;
+`;
+
+const StyledInstagram = styled(Instagram)`
+    margin-right: 30px;
+    color: #FFFF;
+`;
 
 const HeaderOptions = () => {
+    // const { t } = useTransition();
     return (
-        <div className="headerOptions">
-            <a href='/'>Home</a>
-            <a href='/recipies'>Recipies</a>
-            <a href='/about'>About</a>
-            <div className="socialIcons">
-                <Facebook />
-                <Instagram />
-            </div>
-        </div>
+        <StyledHeaderOptions>
+           <StyledNavBar>
+              <StyledHeaderItem href='/'>{headerOptionsMenu.menuItem_1}</StyledHeaderItem>
+              <StyledHeaderItem href='/recipies'>{headerOptionsMenu.menuItem_2}</StyledHeaderItem>
+              <StyledHeaderItem href='/about'>{headerOptionsMenu.menuItem_3}</StyledHeaderItem>
+           </StyledNavBar>
+            <StyledSocialIcons>
+                <StyledFacebook />
+                <StyledInstagram />
+            </StyledSocialIcons>
+        </StyledHeaderOptions>
     )
 }
 
